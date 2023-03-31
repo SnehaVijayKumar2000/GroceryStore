d26af2654ae1df3dfa909cf67de3571890707988
Sneha Vijay Kumar <62722033+SnehaVijayKumar2000@users.noreply.github.com>
2023-03-30 20:25:34 -0400

    Add files via upload

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyByiB4amcQOlaq_fe7KReziOwbz2fnaiIY",
+    authDomain: "grocery-store-b4750.firebaseapp.com",
+    databaseURL: "https://grocery-store-b4750-default-rtdb.firebaseio.com",
+    projectId: "grocery-store-b4750",
+    storageBucket: "grocery-store-b4750.appspot.com",
+    messagingSenderId: "325024488638",
+    appId: "1:325024488638:web:98ac7020b65af05b5652ef",
+    measurementId: "G-TXM5QXMQFB"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,27 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyByiB4amcQOlaq_fe7KReziOwbz2fnaiIY",
+    authDomain: "grocery-store-b4750.firebaseapp.com",
+    databaseURL: "https://grocery-store-b4750-default-rtdb.firebaseio.com",
+    projectId: "grocery-store-b4750",
+    storageBucket: "grocery-store-b4750.appspot.com",
+    messagingSenderId: "325024488638",
+    appId: "1:325024488638:web:98ac7020b65af05b5652ef",
+    measurementId: "G-TXM5QXMQFB"
+}
+}
+
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
